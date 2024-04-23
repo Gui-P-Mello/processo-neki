a56eeb2fb5cb4ea37ad058e221965400fcefe1b3
Guilherme Pereira Mello <guiice3@gmail.com>
2024-04-22 23:15:56 -0300

    Fix: Alteração no nome das sequências criadas no script sql.

--- SistemaSkill.sql
+++ SistemaSkill.sql
@@ -1,32 +1,32 @@
-CREATE SEQUENCE id_pessoa_sequence
+CREATE SEQUENCE pessoas_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
    
-CREATE SEQUENCE id_skill_sequence
+CREATE SEQUENCE skill_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE id_pessoa_skill_sequence
+CREATE SEQUENCE pessoa_skill_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
 create table pessoas(
-	id_pessoa integer primary key default nextval('id_pessoa_sequence'),
+	id_pessoa integer primary key default nextval('pessoas_seq'),
 	nome_pesoa varchar(50) not null,
 	login varchar(20) unique not null,
 	senha varchar(16) not null
 );
 
 create table skills(
-	id_skill integer primary key default nextval('id_skill_sequence'),	
+	id_skill integer primary key default nextval('skill_seq'),	
 	nome_skill varchar(50) unique not null
 );
 
 create table pessoa_skill(
-	id_pessoa_skill integer primary key default nextval('id_pessoa_skill_sequence'),
+	id_pessoa_skill integer primary key default nextval('pessoa_skill_seq'),
 	id_pessoa_f_key integer not null,
 	id_skill_f_key integer not null,
 	foreign key (id_pessoa_f_key) references pessoas(id_pessoa),
